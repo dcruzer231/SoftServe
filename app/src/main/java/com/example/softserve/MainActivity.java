05af61fe626731c46048849992c8898943d29c15
Noah Padilla <44614175+NoahTheGr8@users.noreply.github.com>
2020-02-01 10:06:46 -0700

    Placed a new comment

--- app/src/main/java/com/example/softserve/MainActivity.java
+++ app/src/main/java/com/example/softserve/MainActivity.java
@@ -22,7 +22,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
-//noah
+//noahs new changes
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
